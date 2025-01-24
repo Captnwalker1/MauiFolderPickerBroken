22087f5cf3ecd43fe897127c1a5a60920fb5a864
Walker, Chris <32341@nelnet.net>
2025-01-24 15:21:35 -0500

    Add project files.

---
+++ MauiFolderPickerBroken/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiFolderPickerBroken
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiFolderPickerBroken/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Maui.Storage;
+
+namespace MauiFolderPickerBroken
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+#if WINDOWS
+            var result = await FolderPicker.Default.PickAsync(CancellationToken.None);
+            if (result.IsSuccessful)
+            {
+                PathLabel.Text = "Folder Path: " + result.Folder.Path;
+            }
+            else
+            {
+                PathLabel.Text = "Folder Path: Error: "+result.Exception.Message;
+            }
+            SemanticScreenReader.Announce(CounterBtn.Text);
+#endif
+        }
+    }
+
+}
